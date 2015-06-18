834ac7b8ef95da5ffb6af0849badc613bd54698d
Saleksei <info@mlight.com.ua>
2015-06-18 15:38:50 +0300

    Init Commit

---
+++ GoJS/Lesson-4/task-2.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Алексей on 18.06.2015.
+ */
+/**Напишите функцию checkSpam которая проверяет строку на содержание слов: spam, sex.
+
+ checkSpam('get new Sex videos'); // true
+ checkSpam('[SPAM] How to earn fast money?'); // true
+ checkSpam('New PSD template'); // false
+ */
+
+checkSpam('sex');
+
+function checkSpam(spamOrNot){
+
+    spamOrNot = spamOrNot.toLowerCase();
+
+    if (~spamOrNot.indexOf("sex")) {
+        return false;
+    }
+    else if (~spamOrNot.indexOf("spam")) {
+        return false;
+    }
+    else  return true;
+}
